eff5b6047a379fba80d2a5aad371502ade7e1b5b
Seppe De Langhe <seppedelanghe@gmail.com>
2022-11-05 21:57:41 +0100

    add SDL2_ttf and fix warning

--- Game.cpp
+++ Game.cpp
@@ -98,8 +98,6 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
 }
 
 
-
-
 auto& tiles(manager.getGroup(Game::groupMap));
 auto& players(manager.getGroup(Game::groupPlayers));
 auto& colliders(manager.getGroup(Game::groupColliders));
